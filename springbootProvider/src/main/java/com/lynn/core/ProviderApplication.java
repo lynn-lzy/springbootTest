d486b8cdbddc7e2f90fae99d7101418cbfb61dd6
AlonelyEagle <35294018+AlonelyEagle@users.noreply.github.com>
2020-04-22 13:59:54 +0800

    修改springbootProvider主函数入口

--- springbootProvider/src/main/java/com/lynn/core/ProviderApplication.java
+++ springbootProvider/src/main/java/com/lynn/core/ProviderApplication.java
@@ -3,12 +3,17 @@ package com.lynn.core;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableDubboConfiguration
-public class ProviderApplication {
+public class ProviderApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class,args);
     }
 
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(this.getClass());
+    }
 }
