4503c3c28d1ab5c2e55ba1800a83c706ee5c2298
AlonelyEagle <35294018+AlonelyEagle@users.noreply.github.com>
2020-04-22 12:17:57 +0800

    pull provider appliction file

--- springbootProvider/src/main/java/com/lynn/core/ProviderApplication.java
+++ springbootProvider/src/main/java/com/lynn/core/ProviderApplication.java
@@ -1,8 +1,14 @@
 package com.lynn.core;
 
-import sun.applet.Main;
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+@EnableDubboConfiguration
 public class ProviderApplication {
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class,args);
+    }
 
 }
